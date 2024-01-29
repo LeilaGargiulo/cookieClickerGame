bb51cf3c1ace39ce8ce92636d02f512c7a0116d4
LeilaGargiulo <144458769+LeilaGargiulo@users.noreply.github.com>
2024-01-29 14:02:15 +0000

    Nothing

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let score = 0;
 
-document.getElementById('ducky').addEventListener('click', () => {
+document.getElementById("ducky").addEventListener('click', () => {
     score++;
     document.getElementById('score').textContent = `Score: ${score}`;
 });
