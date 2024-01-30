5167e4137125a3c83857dc17cef0ca5047b80ca7
LeilaGargiulo <144458769+LeilaGargiulo@users.noreply.github.com>
2024-01-30 15:03:12 +0000

    Nothing

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 let score = 0;
-
+let upgradeBought = false;
 document.getElementById("ducky").addEventListener('click', () => {
-    score++;
+    if (upgradeBought) {
+        score += 10;
+    } else {
+        score++;
+    }
     document.getElementById('score').textContent = `Score: ${score}`;
 });
+function buyUpgrade() {
+    if (score >= 1000000) {
+        document.getElementById("ducky").src = "https://tse1.mm.bing.net/th/id/OIG2.sKYcPw0wgDD5je.Jiapn?pid=ImgGn";
+        upgradeBought = true;
+        setInterval(() => {
+            score += 10;
+            document.getElementById('score').textContent = `Score: ${score}`;
+        }, 1000);
+    }
+}
